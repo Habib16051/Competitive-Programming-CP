b5bffdbb6c65ef7bcde50ff12f945baea32a174c
Md Habibur Rahman <39822204+Habib16051@users.noreply.github.com>
2025-05-26 11:58:09 +0600

    Update power_func.py

--- power_func.py
+++ power_func.py
@@ -1,12 +1,15 @@
 def power(base, exp):
-    result = 1
-    
+    # Initially we take the final result as 1 by default
+    result = 1    
+
+    # this loop is used based on the exponent! If the exponent we take 3 , this loop will run 3 times and so on
     for _ in range(abs(exp)):
+        # After each execution of this loop, it will multiply the result and base and will store it to the result variable
         result *= base
-        
+        #  if the exponent is negative then it will give 1/result (1/3) = 0.3333
     if exp < 0:
         return 1/result
-        
+        #  and if the exponent is positive number then it will return result. As example : 2**3 = 8 where, two is base, 3 is exponent and 8 is result.
     return result
     
     
